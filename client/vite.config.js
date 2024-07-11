2ef5323d2cda60addc46727f60a2dc90360c3027
MyVirtualCard <ats.myvirtualcard@gmail.com>
2024-07-11 11:54:15 +0530

    proxy updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Expose the server to the network (optional)
+    port: 3001, // Specify the port (optional)
+    proxy: {
+      // Proxy API requests to the backend server
+      '/': {
+        target: import.meta.env.VITE_APP_API_URL, // Replace with your backend server address
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ /, ''), // Remove /api prefix when forwarding to the backend
+      },
+    },
+  },
 })
 
