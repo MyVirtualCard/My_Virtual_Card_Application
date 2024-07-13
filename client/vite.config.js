94dc7108f9d5ccde6eb35147f280e87757b410dc
MyVirtualCard <ats.myvirtualcard@gmail.com>
2024-07-14 00:06:33 +0800

    npm updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['primereact/editor']
+    }
+  },
   plugins: [react()],
 })
 
