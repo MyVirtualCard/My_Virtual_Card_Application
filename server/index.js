bb20e4989c732a491c400649cd51b0bcbb788167
MyVirtualCard <ats.myvirtualcard@gmail.com>
2025-01-23 15:13:45 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use(cors({
   origin: 'https://myvirtualcard.in',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
+  credentials:true
 }));
 // app.use(cors('*'));
 //Razorpay Instantiate:
